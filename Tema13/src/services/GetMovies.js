20c3621f8b7da0f6af9b6a439f371f4770e1ff24
willian.rochagit config --global user.name <willian.rocha@viaflow.com.br>
2020-02-21 10:52:07 -0300

    final

--- Tema13/src/services/GetMovies.js
+++ Tema13/src/services/GetMovies.js
@@ -2,7 +2,6 @@ import api from '../service/api';
 
 const GetMovies = async (data) => {
   return await api.get(`/?apikey=${process.env.REACT_APP_SECRET_KYE}&s=${data}`);
-
 }
 
 export default GetMovies;
